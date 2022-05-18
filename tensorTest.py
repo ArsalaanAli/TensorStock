19299c6424a13616bf97b2ca75873aa7b881aee6
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2022-05-18 00:18:57 -0400

    panda csv file reading

--- tensorTest.py
+++ tensorTest.py
@@ -1,3 +1,21 @@
-import tensorflow as tf
+# import tensorflow as tf
 
-print("tf version", tf.__version__)
+# index - date - close - volume - open - high - low
+
+import pandas as pd
+
+datafile = pd.read_csv(r"./AppleStockData.csv")
+datafile = datafile.drop(columns="Open")
+
+def getPastThreeMonths(data):
+    endIndex = 0
+    startDate = data.iloc(0)[0]["Date"]
+    startMonth = int(startDate[0:2])
+    for row in data.iterrows():
+        curMonth = int(row[1]["Date"][0:2])
+        if curMonth < startMonth - 2:
+            break
+        print(curMonth)
+        endIndex += 1
+    return endIndex
+print("end", getPastThreeMonths(datafile))

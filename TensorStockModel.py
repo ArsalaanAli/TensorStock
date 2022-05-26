4dba67dc409e732f2e8df9ae181e442c035ef7b5
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2022-05-25 20:43:05 -0400

    resetting data formatting

--- TensorStockModel.py
+++ TensorStockModel.py
@@ -2,30 +2,32 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-def build_model(train_data, ):
-  
-  normalizer = tf.keras.layers.Normalization(axis=-1)
-  normalizer.adapt(train_data)
-  model = tf.keras.Sequential([normalizer, tf.keras.layers.Dense(64, activation='relu'),
-      tf.keras.layers.Dense(64, activation='relu'),
-      tf.keras.layers.Dense(1)
+def build_model():
+  model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1, kernel_initializer='normal', activation='relu'),
+    tf.keras.layers.Dense(64, kernel_initializer="normal", activation='relu'),
+    tf.keras.layers.Dense(64, kernel_initializer="normal", activation='relu'),
+    tf.keras.layers.Dense(1)
   ])
-  
+  model.compile(loss="mean_squared_logarithmic_error",
+                optimizer=tf.keras.optimizers.Adam(0.001))
   return model
 
-def stockLoss(val_true, val_pred):
-  print("THISJAHJSKFJSKFHS ==== ===================================================")
-  print(val_true-val_pred)
-  return val_true - val_pred
-
 loadFile = open("trainData.npy", "rb")
-train = np.load(loadFile)
+train = np.load(loadFile)[0]
 loadFile1 = open("labelData.npy", "rb")
-label = np.load(loadFile1)
+label = np.load(loadFile1)[0]
+
+train = np.array([train])
+label = np.array([label])
 
 
-model = build_model(train[0])
-model.compile(loss=stockLoss,
-                optimizer=tf.keras.optimizers.Adam(0.001))
-train = model.fit(train[0:1], label[0:1], epochs=1, verbose=1, )
+print(train, label)
 
+model = build_model()
+
+trained = model.fit(train, label, epochs=1, verbose=1, )
+
+pred = model.predict(train)
+
+print(pred)
